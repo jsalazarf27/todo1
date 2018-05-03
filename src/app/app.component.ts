36b85b62cb5edf3c4360031e89c9375f87639677
t1jsalazar <jsalazar@todo1.com>
2018-05-02 19:45:02 -0500

    Changes in main screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
 	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-	title = 'app';
-	person = 'Homero';
+	title = 'my application';
+	author = 'Jhon Salazar';
 }
